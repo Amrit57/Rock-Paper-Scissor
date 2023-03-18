3ffa1e63f8ad984f7786dab71ecfe0916a304706
Amrit57 <fenpadorje7@gmail.com>
2023-03-18 09:32:00 +0545

    Game runs success, restart btn added.

--- script.js
+++ script.js
@@ -1,84 +1,198 @@
-    // ****************Create a rock paper scissor Game. player VS computer********************
+
+//create btn which selects and matches the user choice.
+const rock = document.getElementById('rock')
+const paper = document.getElementById('paper')
+const scissor = document.getElementById('scissor')
 
 
-//Function which takes  runs the game and takes input from player and computer.
-const playGround = () => {
-
-    //variables to show score.
-    let userScore = 0;
-    let computerScore = 0;
-
-    //Loop to run game for 5 rounds.
-    for (i = 0; i < 5; i++) {
 
 
-        // create a function which let computer choose random option. (ROCK, PAPER OR SCISSOR).
-        const opt = ["ROCK", "PAPER", "SCISSOR"]
-        const computerInput = () => {
-            const randomIndex = Math.floor(Math.random() * opt.length)
-            const computerInput = opt[randomIndex]
-            return computerInput;
-        }
-        const computerChoice = computerInput();
+// Ask input from user which is case insensitive.
+const opt = ["ROCK", "PAPER", "SCISSOR"];
+let playerSelection;
+let text;
+let result;
+let round = 0;
 
 
-        // Ask input from user which is case insensitive.
+//variables to show score.
+let scoreText;
+let userScore = 0;
+let computerScore = 0;
+let draw = 0;
 
-        const playerSelection = prompt("Enter Rock, Paper Or scissor? : ");
-        const userChoice = playerSelection.toUpperCase();
-       
-        // Condition that decides the winner
-        if (userChoice === "ROCK" && computerChoice === "SCISSOR") { userScore++;
-            console.log(`USER : ${userChoice}  AI: ${computerChoice}, USER wins this round.`);
-            
-           
+//Create a div to show the game results.
 
-        } else if (userChoice === "ROCK" && computerChoice === "PAPER") { computerScore++;
-            console.log(`User : ${userChoice}  AI: ${computerChoice}, AI wins this round.`);
-           
-            
+const container = document.querySelector('.container');
+let div = document.createElement('div');
+div.classList = 'result';
+container.appendChild(div);
+const para = document.createElement("p");
+div.appendChild(para);
+const resultPara = document.createElement('p');
+resultPara.classList = 'result-para'
+div.appendChild(resultPara);
+const score = document.createElement('p')
+div.appendChild(score)
 
+//Reset function to restart the game.
+function reset() {
+    resultPara.textContent = '';
+    div.style.display = "none";
+    round = 0;
+    userScore = 0;
+    computerScore = 0;
+    draw = 0;
+    console.log('hello')
 
-        } else if (userChoice === "SCISSOR" && computerChoice === "PAPER") { userScore++
-            console.log(`User : ${userChoice}  AI: ${computerChoice}, USER wins this round.`);
-            let result = userScore;
-           
+};
 
-        } else if (userChoice === "SCISSOR" && computerChoice === "ROCK") { computerScore++
-            console.log(`User : ${userChoice}  AI: ${computerChoice}, AI wins this round.`);
-           
-           
+//Restart button to reset the game.
+const restart = document.querySelector(".restart")
+restart.addEventListener('click', reset,)
 
+//Adding events to the btns.
+rock.addEventListener('click', selectRock);
+function selectRock() {
+    let playerSelection = opt[0];
+    userChoice = playerSelection;
+    game();
 
-        } else if (userChoice === "PAPER" && computerChoice === "ROCK") { userScore++;
-            console.log(`User : ${userChoice}  AI: ${computerChoice}, USER wins this round.`);
-            let result = userScore;
-           
-
-
-        } else if (userChoice === "PAPER" && computerChoice === "SCISSOR") { computerScore++;
-            console.log(`User : ${userChoice}  AI: ${computerChoice}, AI wins this round.`);
-            let result = userScore;
-            
-        }
-        else if (userChoice === computerChoice) {
-            console.log(`User : ${userChoice}  AI: ${computerChoice}, DRAW`);
-        } else if (userChoice !== opt) {
-            console.log('PLEASE CHOOSE ROCK, PAPER OR SCISSOR ONLY.');
-        }
-
-      
-    }
-
-    // condition that shows overall Score.
-    if(userScore == computerScore){
-        console.log(`You lose! AI:${computerScore}  USER: ${userScore}`)
-    } else if(userScore < computerScore){
-        console.log(`You lose! AI:${computerScore}  USER: ${userScore}`)
-    } else{
-        console.log(`You win! AI:${computerScore}  USER: ${userScore}`)
-      
-    }
 }
-playGround()
+
+paper.addEventListener('click', selectPaper);
+function selectPaper() {
+    let playerSelection = opt[1];
+    userChoice = playerSelection;
+    game();
+
+
+
+}
+scissor.addEventListener('click', selectScissor);
+function selectScissor() {
+    let playerSelection = opt[2];
+    userChoice = playerSelection;
+    game();
+
+}
+
+//Function which runs playrgound and shows final result.
+function game() {
+    div.style.display = "block";
+    if (round < 4) {
+        playGround()
+        return;
+    } else {
+        if (userScore > computerScore) {
+            let result = ` You win!`;
+            resultPara.textContent = result;
+        } else if (userScore < computerScore) {
+            let result = ` You lose!`;
+            resultPara.textContent = result;
+
+        } else {
+            let result = ` Draw!`;
+            resultPara.textContent = result;
+        }
+    }
+
+
+}
+
+
+
+//Function which runs the game and takes input from player and computer.
+function playGround() {
+
+    // create a function which let computer choose random option. (ROCK, PAPER OR SCISSOR).
+    const computerInput = () => {
+        const randomIndex = Math.floor(Math.random() * opt.length)
+        const computerInput = opt[randomIndex]
+        return computerInput;
+    }
+    const computerChoice = computerInput();
+
+
+
+
+
+    // Condition that decides the winner
+    if (userChoice === "ROCK" && computerChoice === "SCISSOR") {
+        userScore++;
+        round++;
+        let text = `USER : ${userChoice}  AI: ${computerChoice}, USER wins this round.`;
+        para.textContent = text;
+        let scoreText = `User: ${userScore}  AI: ${computerScore}  Draw: ${draw}`;
+        score.textContent = scoreText;
+
+
+
+
+    } else if (userChoice === "ROCK" && computerChoice === "PAPER") {
+        computerScore++;
+        round++;
+        let text = `User : ${userChoice}  AI: ${computerChoice}, AI wins this round.`;
+        para.textContent = text;
+        let scoreText = `User: ${userScore}  AI: ${computerScore}  Draw: ${draw}`;
+        score.textContent = scoreText;
+
+
+
+    } else if (userChoice === "SCISSOR" && computerChoice === "PAPER") {
+        userScore++
+        round++;
+        let text = `USER : ${userChoice}  AI: ${computerChoice}, USER wins this round.`;
+        para.textContent = text;
+        let scoreText = `User: ${userScore}  AI: ${computerScore}  Draw: ${draw}`;
+        score.textContent = scoreText;
+
+
+
+    } else if (userChoice === "SCISSOR" && computerChoice === "ROCK") {
+        computerScore++
+        round++;
+        let text = `User : ${userChoice}  AI: ${computerChoice}, AI wins this round.`;
+        para.textContent = text;
+        let scoreText = `User: ${userScore}  AI: ${computerScore}  Draw: ${draw}`;
+        score.textContent = scoreText;
+
+
+
+    } else if (userChoice === "PAPER" && computerChoice === "ROCK") {
+        userScore++;
+        round++;
+        let text = `USER : ${userChoice}  AI: ${computerChoice}, USER wins this round.`;
+        para.textContent = text;
+        let scoreText = `User: ${userScore}  AI: ${computerScore}  Draw: ${draw}`;
+        score.textContent = scoreText;
+
+
+
+    } else if (userChoice === "PAPER" && computerChoice === "SCISSOR") {
+        computerScore++;
+        round++;
+        let text = `User : ${userChoice}  AI: ${computerChoice}, AI wins this round.`;
+        para.textContent = text;
+        let scoreText = `User: ${userScore}  AI: ${computerScore}  Draw: ${draw}`;
+        score.textContent = scoreText;
+
+    }
+    else if (userChoice === computerChoice) {
+        round++;
+        draw++;
+        let text = `User : ${userChoice}  AI: ${computerChoice}, DRAW`;
+        para.textContent = text;
+        let scoreText = `User: ${userScore}  AI: ${computerScore}  Draw: ${draw}`;
+        score.textContent = scoreText;
+
+    }
+
+    //Condition that shows result after 5 rounds.
+    console.log(`User: ${userScore}`);
+    console.log(`AI: ${computerScore}`);
+}
+
+
+
 
